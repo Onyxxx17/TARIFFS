609dc27fe7cbfdbb12a2567f83150eaccbbd841a
n0wayy100 <lkpethein@gmail.com>
2025-09-28 18:36:27 +0800

    Minor changes to swagger-ui

--- backend/src/main/java/com/tariff/controller/UserController.java
+++ backend/src/main/java/com/tariff/controller/UserController.java
@@ -16,7 +16,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.Map;
 
-@Hidden
+
 @RestController
 @RequestMapping("/api/users")
 @CrossOrigin(origins = "http://localhost:5173")
@@ -27,20 +27,23 @@ public class UserController {
     public UserController(UserService userService) {
         this.userService = userService;
     }
-
+    
     // --- Get all users ---
+    @Hidden
     @GetMapping
     public List<User> getAllUsers() {
         return userService.listUser();
     }
 
     // --- Get user by ID ---
+    @Hidden
     @GetMapping("/{id}")
     public User getUserById(@PathVariable Long id) {
         return userService.getUser(id);
     }
 
     // --- Get user by username ---
+    @Hidden
     @GetMapping("/username/{username}")
     public ResponseEntity<User> getUserByUsername(@PathVariable String username) {
         Optional<User> user = userService.getUserByUsername(username);
@@ -49,18 +52,21 @@ public class UserController {
     }
 
     // --- Create user ---
+    @Hidden
     @PostMapping
     public User createUser(@RequestBody User user) {
         return userService.addUser(user);
     }
 
     // --- Update user ---
+    @Hidden
     @PutMapping("/{id}")
     public User updateUser(@PathVariable Long id, @RequestBody User user) {
         return userService.updateUser(id, user);
     }
 
     // --- Delete user ---
+    @Hidden
     @DeleteMapping("/{id}")
     public ResponseEntity<?> deleteUser(@PathVariable Long id) {
         userService.deleteUser(id);
