3b65814a155db6a4a25eb76b7b88823aab25cb94
Aung Ye Thant Hein <aungyethant@gmail.com>
2025-09-30 01:13:53 +0800

    feat: update API endpoints to use BASE_URL and enhance service management in CI/CD

--- backend/src/main/java/com/tariff/config/WebConfig.java
+++ backend/src/main/java/com/tariff/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("http://localhost:5173","https://tarifffic.app","www.tarifffic.app","tarriff-xi.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
